6e89fee8af25b13338c0beec6e29162182a05170
kevin <castaneda.kevin114@gmail.com>
2018-02-27 20:40:14 -0500

    First commit

---
+++ src/pages/menu-dishes/menu-dishes.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'menu-dishes',
+  templateUrl: 'menu-dishes.component.html'
+})
+export class MenuDishesComponent {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
